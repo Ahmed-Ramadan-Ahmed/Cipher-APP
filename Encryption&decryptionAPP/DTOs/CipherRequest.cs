105f186afb65dba06d218ed967a57f5db47b5f09
Ahmed Ramadan <aramadan442000@gmail.com>
2025-02-22 13:51:39 +0200

    Backend added

---
+++ Encryption&decryptionAPP/DTOs/CipherRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Encryption___decryption_APP.DTOs
+{
+    public class CaesarRequest
+    {
+        [Required]
+        public string PlainText { get; set; }
+        
+        [Required]
+        public string Shift { get; set; }
+    }
+}
